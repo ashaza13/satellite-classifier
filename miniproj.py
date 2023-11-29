ed2cdea490d99a851d6cef148b7ad2d83b3671b4
walsh968 <121217233+walsh968@users.noreply.github.com>
2023-11-29 14:43:49 -0500

    Add files via upload

--- miniproj.py
+++ miniproj.py
@@ -19,18 +19,12 @@ count = 0
 for filename in os.listdir("images"):
     path = os.path.join("images", filename)
     img = cv2.imread(path)
-    if count < 18000:
+    if count < 24000:
         labels.append(0)
-        count += 1
-    elif count < 24000:
-        labels.append(0)
-        count += 1
-    elif count < 30000:
-        labels.append(1)
-        count += 1
     else:
         labels.append(1)
     all2test.append(img)
+    count += 1
     
 images = numpy.array(all2test)
 newLabels = numpy.array(labels)
@@ -39,13 +33,17 @@ images_flattened = images.reshape(images.shape[0], -1)
 
 images_train, images_test, labels_train, labels_test = train_test_split(images_flattened, newLabels, test_size=0.2, shuffle=True, stratify=newLabels)
 
+
+'''
 classifier = SVC()
 
-parameters = [{'gamma': [0.01, 0.1, 1], 'C': [1, 10, 100, 1000]}]
+print('before training')
+parameters = [{'gamma': [0.01], 'C': [1]}]
 
 grid_search = GridSearchCV(classifier, parameters)
 
 grid_search.fit(images_train, labels_train)
+print('passed grid stuff')
 
 best_estimator = grid_search.best_estimator_
 
@@ -54,6 +52,7 @@ prediction = best_estimator.predict(images_test)
 score = accuracy_score(prediction, labels_test)
 
 print('{}% of samples were correctly classified'.format(str(score * 100)))
+'''
 
 
 
